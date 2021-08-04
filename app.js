ab87127c87641810c81ca5ec1316b7d1543a47b1
Coral Herrnandez <45517870+Esmeralda1997@users.noreply.github.com>
2021-08-04 14:02:22 -0500

    Added todays date

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-//get date
+//localStorage 
+//arr, advice, and date
 
+
+//get date
+const date = new Date();
+const [month, day, year, weekday] = [date.getMonth(), date.getDate(), date.getFullYear(), date.getDay()];
+const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+document.querySelector('#date').textContent = `${weekdays[weekday]} ${months[month]} ${day}, ${year}`;
 
 //get API --> make it way more seamless
 $.getJSON("https://api.adviceslip.com/advice", function(data) {
-    document.querySelector('#advice').textContent = data.slip.advice;
+    let advice = data.slip.advice;
+    document.querySelector('#advice').textContent = advice;
 })
+
+//store quote in localStorage
+
+
+//refresh API 
